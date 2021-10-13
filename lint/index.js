a1d7fee05006b9e2b76012f8dc41c1e27604d35d
Chris Carrillo <chriscarrillo@outlook.com>
2021-10-13 15:47:07 -0500

    Upgraded Outdated Packages

--- lint/index.js
+++ lint/index.js
@@ -32,9 +32,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-    'prettier/react',
+    'prettier',
   ],
   overrides: [
     {
